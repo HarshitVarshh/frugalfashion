99996f55811c95111e7584f8ae3f0c01bad1c0e1
HarshitVarshh <54776262+HarshitVarshh@users.noreply.github.com>
2023-11-18 15:14:34 +0530

    add images to the card

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,27 +1,195 @@
 import React from "react";
 import Card from "./Card";
+import logo1 from '../Images/websitematterwithpictures/Picture1.jpg';
+import logo2 from '../Images/websitematterwithpictures/Picture2.png';
+import logo3 from '../Images/websitematterwithpictures/Picture3.png';
+import logo4 from '../Images/websitematterwithpictures/Picture4.jpg';
+import logo5 from '../Images/websitematterwithpictures/Picture5.png';
+import logo6 from '../Images/websitematterwithpictures/Picture6.jpg';
+import logo7 from '../Images/websitematterwithpictures/Picture7.png';
+import logo8 from '../Images/websitematterwithpictures/Picture8.jpg';
+import logo9 from '../Images/websitematterwithpictures/Picture9.png';
+import logo10 from '../Images/websitematterwithpictures/Picture10.jpg';
+import logo11 from '../Images/websitematterwithpictures/Picture11.png';
+import logo12 from '../Images/websitematterwithpictures/Picture12.png';
+import logo13 from '../Images/websitematterwithpictures/Picture13.png';
+import logo14 from '../Images/websitematterwithpictures/Picture14.png';
+import logo15 from '../Images/websitematterwithpictures/Picture15.jpg';
+import logo16 from '../Images/websitematterwithpictures/Picture16.png';
+import logo17 from '../Images/websitematterwithpictures/Picture17.png';
+import logo18 from '../Images/websitematterwithpictures/Picture18.jpg';
+import logo19 from '../Images/websitematterwithpictures/Picture19.jpg';
+import logo20 from '../Images/websitematterwithpictures/Picture20.jpg';
+import logo21 from '../Images/websitematterwithpictures/Picture21.png';
+import logo22 from '../Images/websitematterwithpictures/Picture22.png';
+import logo23 from '../Images/websitematterwithpictures/Picture23.jpg';
+import logo24 from '../Images/websitematterwithpictures/Picture24.jpg';
+import logo25 from '../Images/websitematterwithpictures/Picture25.jpg';
+import logo26 from '../Images/websitematterwithpictures/Picture26.jpg';
 
 function AboutUs() {
   var cards_data = [
     {
       title: "Vero est similique",
+      img: logo1,
       content:
         "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
     },
     {
       title: "Vero est similique",
+      img: logo2,
       content:
         "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
     },
     {
       title: "Vero est similique",
+      img: logo3,
       content:
         "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
     },
+    {
+      title: "Vero est similique",
+      img: logo4,
+      content:
+        "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
+    },
+    {
+      title: "Vero est similique",
+      img: logo5,
+      content:
+        "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
+    },
+    {
+      title: "Vero est similique",
+      img: logo6,
+      content:
+        "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
+    },
+    {
+      title: "Vero est similique",
+      img: logo7,
+      content:
+        "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
+    },
+    {
+      title: "Vero est similique",
+      img: logo8,
+      content:
+        "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
+    },
+    {
+      title: "Vero est similique",
+      img: logo9,
+      content:
+        "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
+    },
+    {
+      title: "Vero est similique",
+      img: logo10,
+      content:
+        "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
+    },
+    {
+      title: "Vero est similique",
+      img: logo11,
+      content:
+        "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
+    },
+    {
+      title: "Vero est similique",
+      img: logo12,
+      content:
+        "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
+    },
+    {
+      title: "Vero est similique",
+      img: logo13,
+      content:
+        "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
+    },
+    {
+      title: "Vero est similique",
+      img: logo14,
+      content:
+        "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
+    },
+    {
+      title: "Vero est similique",
+      img: logo15,
+      content:
+        "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
+    },
+    {
+      title: "Vero est similique",
+      img: logo16,
+      content:
+        "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
+    },
+    {
+      title: "Vero est similique",
+      img: logo17,
+      content:
+        "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
+    },
+    {
+      title: "Vero est similique",
+      img: logo18,
+      content:
+        "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
+    },
+    {
+      title: "Vero est similique",
+      img: logo19,
+      content:
+        "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
+    },
+    {
+      title: "Vero est similique",
+      img: logo20,
+      content:
+        "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
+    },
+    {
+      title: "Vero est similique",
+      img: logo21,
+      content:
+        "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
+    },
+    {
+      title: "Vero est similique",
+      img: logo22,
+      content:
+        "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
+    },
+    {
+      title: "Vero est similique",
+      img: logo23,
+      content:
+        "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
+    },
+    {
+      title: "Vero est similique",
+      img: logo24,
+      content:
+        "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
+    },
+    {
+      title: "Vero est similique",
+      img: logo25,
+      content:
+        "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
+    },
+    {
+      title: "Vero est similique",
+      img: logo26,
+      content:
+        "Vero est similique sunt, fugitreprehenderit vel aliquid nobis, sequi accusamus ad expedita, autemmaxime ipsam error asperiores ratione quis!",
+    }
   ];
   return (
     <div id="aboutus" className="body_content">
       <h1>Our Story</h1>
+      <img></img>
       <br />
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni molestiae
@@ -50,12 +218,14 @@ function AboutUs() {
       </p>
       <br />
       <br />
-      <div className="card_container">
+    <div >
         {cards_data.map((cd) => {
-          return <Card title={cd.title} content={cd.content} />;
+          return (<div><p><Card title={cd.title} content={cd.content} img={cd.img} /> </p></div>
+          );
         })}
       </div>
-    </div>
+  </div>
+    
   );
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Card.css'
 
-function Card({title,content}) {
+function Card({title,content,img}) {
   return (
     <div className='card' >
         <h4>{title}</h4>
         <br/>
         <p>{content}</p>
+        <img src={img} />
     </div>
   )
 }
